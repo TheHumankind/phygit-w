8960a96dfcb9186fd4d32343b6781e09d17f6c14
Hurin Vlad <longsno@gmail.com>
2023-02-20 21:02:17 +0300

    fix: header fix

--- js/header.js
+++ js/header.js
@@ -1,7 +1,15 @@
 const burgerItem = document.getElementById('burger');
 const navMenu = document.getElementById('navMenu');
+const headerNavItems = document.querySelectorAll('.header-nav-item');
 
 burgerItem.addEventListener('click', () => {
     burgerItem.classList.toggle('burger-cross')
     navMenu.classList.toggle('header-on');
+})
+
+headerNavItems.forEach((e) => {
+    e.addEventListener('click', () => {
+        burgerItem.classList.remove('burger-cross')
+        navMenu.classList.remove('header-on');
+    })
 })
